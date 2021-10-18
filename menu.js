c90c8d868f99f4b8de06629d8a0438b60549eb8b
a19axechacan <a19axechacan@inspedralbes.cat>
2021-10-18 12:49:22 +0200

    menu.php permet editar un menu i enviar-lo a validacio.php en format json

---
+++ menu.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+
+
+    document.getElementById("formMenu").addEventListener("click", function (e) {
+
+        if (e.target.classList.contains("suma")) {
+            sumarProducte(e.target.parentNode.id);
+        } else if (e.target.classList.contains("resta")) {
+            restarProducte(e.target.parentNode.id);
+
+        }
+    });
+
+
+
+    let buttonComprar = document.getElementById("comprar");
+
+    buttonComprar.addEventListener('click', function () {
+        let inputs = document.getElementsByTagName("input");
+
+        let compra = [];
+
+        for (let index = 0; index < inputs.length; index++) {
+
+
+            let item = {
+                "nom": inputs[index].name,
+                "id": inputs[index].id,
+                "quantitat": inputs[index].value
+            }
+
+            if (item.quantitat > 0) {
+                compra.push(item);
+            }
+        }
+
+        let json = JSON.stringify(compra);
+
+        document.getElementById("json").value = json;
+        document.getElementById("formMenu").submit();
+
+    });
+
+
+
+
+    function sumarProducte(id) {
+        let input = document.querySelector("input[ id='" + id + "']");
+        input.value++;
+    }
+
+    function restarProducte(id) {
+        let input = document.querySelector("input[ id='" + id + "']");
+
+        if (input.value > 0) {
+            input.value = input.value - 1;
+        }
+
+    }
+
+
+
+}
--- menu.php
+++ menu.php
@@ -4,10 +4,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Menu</title>
+    <script src="menu.js"></script>
+
+
 </head>
 <body>
     <h1>menu</h1>
-    <a href="validacio.php">IR A COMPRAR</a>
+    
+
+    <?php 
+    
+    
+    $menu = [
+        0=>[
+            "id"=> 0 ,
+            "nom"=>"Entrepà de fuet" ,
+            "descripció"=> "Entrepà de fuet amb tomàquet",
+            "imatge"=>"adsadas",
+            "preu"=> 1.50  
+        ],
+        1=>[
+            "id"=> 1 ,
+            "nom"=>"Entrepà de formatge" ,
+            "descripció"=> "Entrepà de formatge amb tomàquet",
+            "imatge"=>"adsadas",
+            "preu"=> 1.50  
+        ],
+        2=>[
+            "id"=> 2 ,
+            "nom"=>"Donut" ,
+            "descripció"=> "Donut amb sucre",
+            "imatge"=>"adsadas",
+            "preu"=> 0.60  
+        ],
+        3=>[
+            "id"=> 3 ,
+            "nom"=>"Cafè" ,
+            "descripció"=> "Cafè sol",
+            "imatge"=>"adsadas",
+            "preu"=> 1.20
+        ],
+        4=>[
+            "id"=> 4 ,
+            "nom"=>"Suc de taronja" ,
+            "descripció"=> "Suc de taronja petit",
+            "imatge"=>"adsadas",
+            "preu"=> 2  
+        ],
+        5=>[
+            "id"=> 5 ,
+            "nom"=>"Suc de pinya" ,
+            "descripció"=> "Suc de pinya petit",
+            "imatge"=>"adsadas",
+            "preu"=> 1.85 
+        ]
+        ];
+
+    
+    $form ="";
+    foreach ($menu as $element){
+        $form .= "  <div id=".$element['id'].">"."
+                    <label for=".$element['id'].">".$element['nom'].":</label>
+                   <button type='button' class='resta'>-</button> 
+                   <input type='text' id=".$element['id']." name='".$element['nom']."' value='0'>
+                   <button type='button' class='suma'>+</button> 
+                   <br><br>
+                   </div>
+                   ";
+    }
+    
+    $form .= "<input type='hidden' id ='json'>";
+
+    echo "<form id='formMenu' action='validacio.php' method='get'> ". $form . " <button type='button' id='comprar'>Comprar </button>  </form>";
+    ?>
+
 </body>
 </html>
