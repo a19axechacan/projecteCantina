34e2b056c729c89516ac5f9b3ca5b1cf67d2a0a1
a19axechacan <a19axechacan@inspedralbes.cat>
2021-10-28 17:55:11 +0200

    Missatge de confirmació a l'hora d'intentar esborrar un element del menú

--- admin/adminMenu.js
+++ admin/adminMenu.js
@@ -5,7 +5,11 @@ window.onload = function (){
     document.getElementById("menu-container").addEventListener("click", function (e) {
         if (e.target.classList.contains("buttonEliminar")) {
           inputEliminarProducte.value = e.target.id;
-          formEliminarProducte.submit();
+
+          if(confirm("Segur que vols eliminar aquest element del menú?")){
+              formEliminarProducte.submit();
+          }else alert("Acció cancelada");
+
         }
     });
 
