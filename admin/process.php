ba2e0599bf2cbc6072b144b4d22e8d7a77ec9b7b
a19axechacan <a19axechacan@inspedralbes.cat>
2021-10-28 11:50:50 +0200

    creada finestra adminMenu.php on es pot crear un nou producte i afegir-lo al menu original

---
+++ admin/process.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$nouProducteHorari = $_POST["horariProducte"];
+
+
+$menuFile = fopen("../menu.json", "r");
+$menuRead = fread($menuFile, filesize("../menu.json"));
+fclose($menuFile);
+$arrayMenus = json_decode($menuRead, true);
+
+
+$producteNou = array(
+    "id"=>77,
+    "nom"=>utf8_encode($_POST["nomProducte"]),
+    "descripció"=>utf8_encode($_POST["descProducte"]),
+    "imatge"=>"css/menu/150.png",
+    "preu"=> $_POST["preuProducte"]
+);
+
+
+
+if(strcmp($nouProducteHorari, "dia")){
+    array_push($arrayMenus["dia"],$producteNou);
+}else{
+    array_push($arrayMenus["tarde"],$producteNou);
+}
+
+$jsonMenu = json_encode($arrayMenus, JSON_UNESCAPED_UNICODE);
+
+$filename= "../menu.json";
+touch($filename);
+$file = fopen($filename, "w");
+fwrite($file,$jsonMenu);
+fclose($file);
+
+
+
+
+
+
+
+?>
