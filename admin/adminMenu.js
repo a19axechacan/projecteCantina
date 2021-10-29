400073ef593854d18bfb4f0cac4251aa46a5bd47
SergioooV <a20servilrac@inspedralbes.cat>
2021-10-29 12:45:03 +0200

    obligacions nouproducte

--- admin/adminMenu.js
+++ admin/adminMenu.js
@@ -1,11 +1,12 @@
 window.onload = function (){
 
+    let buttonAfegir= document.getElementById("buttonAfegir");
+
     let formEliminarProducte = document.getElementById("eliminarProducteForm");
     let inputEliminarProducte=  document.getElementById("selectedId");
     document.getElementById("menu-container").addEventListener("click", function (e) {
         if (e.target.classList.contains("buttonEliminar")) {
           inputEliminarProducte.value = e.target.id;
-
           if(confirm("Segur que vols eliminar aquest element del menú?")){
               formEliminarProducte.submit();
           }else alert("Acció cancelada");
@@ -13,7 +14,20 @@ window.onload = function (){
         }
     });
 
+    let producteNouForm= document.getElementById("producteNouForm");
+    let nomProducte= document.getElementById("nomProducte");
+    let descProducte= document.getElementById("descProducte");
+    let preuProducte= document.getElementById("preuProducte");
+    let foto= document.getElementById("foto");
 
+    buttonAfegir.addEventListener("click", function(){
+        if(nomProducte.value!="" && descProducte.value!="" && (preuProducte.value!="" && isNaN(preuProducte.value)== false) && foto.value!=""){
+            producteNouForm.submit();
+    }
+    else {
+        alert("Dades invàlides");
+    }
+    })
 
 
 }
