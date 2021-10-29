f0e0befa384432ffe2bce722cdc9e5f645647d6b
a19axechacan <a19axechacan@inspedralbes.cat>
2021-10-29 12:26:52 +0200

    Fix rutes css a adminMenu.php i admin.php

--- admin/adminMenu.php
+++ admin/adminMenu.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="projecteCantina/css/capçalera.css" rel="stylesheet" type="text/css">
+    <link href="projecteCantina/capçalera.css" rel="stylesheet" type="text/css">
     <link href="adminMenu.css" rel="stylesheet" type="text/css">
     <script src="adminMenu.js"></script>
     <title>Administar menú</title>
